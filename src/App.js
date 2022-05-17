acb0b3a7c34571a496a5f4664ba50ace26e2bb05
Rob Walker <rnwalker7@gmail.com>
2022-05-17 16:24:44 -0500

    Testing some stuff out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { CrosswordProvider, CrosswordGrid, DirectionClues } from '@jaredreisinger/react-crossword';
+
+const data = {
+    across: {
+        1: {
+            clue: 'African or ________ swallow',
+            answer: 'EUROPEAN',
+            row: 2,
+            col: 0
+        },
+        3: {
+            clue: 'Foo',
+            answer: 'BAR',
+            row: 4,
+            col: 1
+        }
+    },
+    down: {
+        2: {
+            clue: 'Opposite of rural',
+            answer: 'URBAN',
+            row: 2,
+            col: 1
+        },
+        4: {
+            clue: 'Mountainous speakers',
+            answer: 'ALPINE',
+            row: 0,
+            col: 4
+        }
+    }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CrosswordProvider data={data}>
+        <div style={{ display: 'flex', gap: '2em' }}>
+          <DirectionClues direction="across" />
+          <div style={{ width: '33vw'}}>
+            <CrosswordGrid />
+          </div>
+          <DirectionClues direction="down" />
+        </div>
+      </CrosswordProvider>
     </div>
   );
 }
